27ab9cfb05249fec29e6777261df63d8682e9363
hashika <hashi.inova@gmail.com>
2023-03-02 12:20:05 +0530

    register auth

--- movie-font/src/redux/reducer/auth.js
+++ movie-font/src/redux/reducer/auth.js
@@ -1,17 +1,24 @@
 const initialesState = {
   data: null,
   isAuthenticated: false,
+  isRegistered: false,
 };
 
 export default function (state = initialesState, action) {
   switch (action.type) {
-    case "USER_REGISTERED":
     case "USER_LOGGED":
       return {
         ...state,
         token: action.payload.accessToken,
         data: action.payload,
         isAuthenticated: true,
+        isRegistered: true,
+      };
+    case "USER_REGISTERED":
+      return {
+        ...state,
+        data: action.payload,
+        isRegistered: true,
       };
     case "LOGIN_FAIL":
       return {
