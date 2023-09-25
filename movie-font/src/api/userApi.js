38d49807db70e1450d6c46c20a775760bb64e264
hashika1996 <m.g.hashikamaduranga@gmail.com>
2023-09-25 15:27:33 +0530

    validate response

--- movie-font/src/api/userApi.js
+++ movie-font/src/api/userApi.js
@@ -22,6 +22,33 @@ userClient.interceptors.request.use(
   }
 );
 
+userClient.interceptors.response.use(
+  (response) => {
+    if (response.headers["refresh-token"]) {
+      this.authService.setAuthToken(response.headers["refresh-token"]);
+      // event.dispatch(ON_JWT_REFRESHED);
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 403) {
+      // event.dispatch(ON_JWT_EXPIRED);
+      localStorage.clear();
+      return (window.location.href = "/signin");
+      return Promise.reject(error);
+    }
+    if (error.response.status === 500 && error.response.data?.message === "internal_server_error") {
+      // event.dispatch(ON_SERVER_ERROR);
+      return Promise.reject(error);
+    }
+    if (error?.code === "ERR_NETWORK") {
+      // event.dispatch(ON_NETWORK_ERROR);
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export const userLogin = (email, password) => {
   return userClient.post(`auth/signin`, {
     email,
