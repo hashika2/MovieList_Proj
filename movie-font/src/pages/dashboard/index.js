972d6fa6b39e1538667706414ab001719e22bdc0
hashika1996 <m.g.hashikamaduranga@gmail.com>
2023-09-27 19:00:17 +0530

    format

--- movie-font/src/pages/dashboard/index.js
+++ movie-font/src/pages/dashboard/index.js
@@ -16,7 +16,7 @@ const Dashboard = () => {
                 // style={{"top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;"}}
                 allowfullscreen
                 ></iframe>
-      </div>
+            </div>
         </div>
     )
 }
